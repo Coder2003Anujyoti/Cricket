3f8fd4d55f027b5038b0beb32bdf3982aa6450e1
Coder2003Anujyoti <anujyotide@gmail.com>
2025-07-29 07:02:32 +0530

    Updated Socket

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,3 +1,4 @@
 // socket.js
 import { io } from "socket.io-client";
-export const socket = io("https://intelligent-ailyn-handcricket-e8842259.koyeb.app/"); // or your backend URL
+export const socket = io("https://intelligent-ailyn-handcricket-e8842259.koyeb.app/",{
+  transports: ["websocket"], }); // or your backend URL
